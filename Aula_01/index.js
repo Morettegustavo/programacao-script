da8ca2007375227261bc1eac9935fc6c7a795ec3
Gustavo Faria Morette <88351614+Morettegustavo@users.noreply.github.com>
2023-02-15 22:01:21 -0300

    redme

--- Aula_01/index.js
+++ Aula_01/index.js
@@ -5,4 +5,18 @@ const express = require('express');
 const app = express();
 
 // Chamar a porta do servidor
-app.listen(8080);
+app.listen(8080, () => {
+    console.log('O servidor está ativo na porta 8080!!!')
+});
+
+// Criando a primeira rota
+app.get('/', () => {
+    console.log('Opa deu certo o acesso!');
+});
+
+// Rota de envio de uma marcação 
+let nome = 'Gustavo Morette';
+app.get("/testeHTML", (req, res) => {
+    res.send(`<h1 style="color: red">Olá ${nome} </h1>`);
+});
+
